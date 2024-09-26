84553cf7eb30f28dd4e2c5da471de25ef5b2c46c
helena-05 <161738204+helena-05@users.noreply.github.com>
2024-09-26 09:24:03 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+// os farofeiro, 12, comédia
+// Guardiões da galáxia, 12, fantasia, aventura, comédia, ficção cientifica
+// minha mãe é uma peça2, 12, comédia
+// homem-aranha sem volta para casa, 12, ação, fantasia, ficção cientifica, super-herói
+// homem-formiga e a vespa, 12, ação, aventura, ficção cientifica, super-herói
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
